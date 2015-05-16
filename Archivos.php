ed496abd02ba60b3b04ed0d5276e7d1f9ec1f099
xblacklotus <carlosleiva95@hotmail.com>
2015-05-16 00:43:59 -0600

    se pueden compartir los links

--- Archivos.php
+++ Archivos.php
@@ -9,10 +9,27 @@
 			$cont=new Controlador1();
 			$cont->subirArchivo();
 		}
+
+		$cont=new Controlador1();
+		$datos=$cont->consultarArchivos();
+
+		foreach ($datos as $value) {
+			?>
+			<form role="form" class="form" action="archivo.php?archivo=<?php echo $value["id_archivo"]; ?>" method="post" autocomplete="off" enctype="multipart/form-data">
+				<?php echo $value["nombre"];?>
+				<input type="hidden" name="nombre" value="<?php echo $value["nombre"];?>">
+				<input type="hidden" name="archivo" value="">
+				<input type="submit" name="Subir" value="Descargar" class="btn btn-primary btn-border btn-lg"/>
+			</form>
+			<?php
+		}
 		?>
 		<blockquote>
 			<form role="form" class="form" action="Archivos.php" method="post" autocomplete="off" enctype="multipart/form-data">
 				<h2 class="form-heading">Archivo:</h2>
+				<div class="form-group">
+					<input type="text" name="descripcion" class="form-control input-lg" required id="txtTipo"  placeholder="Descripción">
+				</div>
 				<div class="form-group">
 					<input type="file" name="archivo" class="form-control input-lg" required id="txtTipo"  placeholder="Archivo">
 				</div>
---
+++ archivo.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.inc'; ?>
+<body>
+	<div class="container">
+		<?php include 'includes/nav.inc'; ?>
+		<h2>Bienvenido a File Manager</h2>
+		<blockquote>
+			<form role="form" class="form" action="download.php" method="post" autocomplete="off" enctype="multipart/form-data">
+				<h2>
+					<?php 
+					if(isset($_GET["archivo"]))
+					{
+						$cont=new Controlador1();
+						$nombre=$cont->getNombre($_GET["archivo"]);
+						echo $nombre['nombre'];
+						?>
+					</h2>
+					<input type="hidden" name="archivo" value="<?php echo $_GET["archivo"]; ?>">
+					<input type="submit" name="Subir" value="Descargar" class="btn btn-primary btn-border btn-lg"/>
+				</form>
+				<?php
+			}
+			?>
+		</blockquote>
+		<?php include 'includes/footer.inc'; ?>
+		<?php include 'includes/scripts.inc'; ?>
+	</div>
+</body>
+</html>
